05f8db0df98f645671c609400f905c9e4aa4b34e
Fahrenheit <igor.maisyk@gmail.com>
2019-04-28 11:22:24 +0300

    Initial Commit

---
+++ FetchResults/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FetchResults
+//
+//  Created by Игорь Майсюк on 4/28/19.
+//  Copyright © 2019 Игорь Майсюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
